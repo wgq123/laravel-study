580a1d3cd07df95da8c5fc27ff1b01fbf59e5f6e
WangGuoQiang <541554093@qq.com>
2022-06-16 00:21:06 +0800

    数据库操作研究

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -2,16 +2,58 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
 class StudyController extends Controller
 {
     public function index()
     {
-        $res = DB::table('admin_users')->get();
-        return response()->json($res);
-        echo "<pre />"; print_r($res);die;
+        /********************************* 查询构造器  **********************************/
+        // 参考链接：
+        //https://laravel.com/docs/8.x/queries
+        //http://t.zoukankan.com/bushui-p-14399217.html
+        //查询所有
+//        $res = DB::table('admin_menu')->get();
+//        echo "<pre />"; print_r($res);die;
+//        return response()->json($res);
+
+        //查询一行
+//        $user = DB::table('admin_menu')->where('title', '首页')->first();
+//        echo "<pre />"; print_r($user);die;
+
+        //查询单个值
+//        $user = DB::table('admin_menu')->where('title', '首页')->value('title');
+//        echo "<pre />"; print_r($user);die;
+
+        //通过id检索整行
+//        $user = DB::table('admin_menu')->find(3);
+//        echo "<pre />"; print_r($user);die;
+
+
+//        该get方法返回一个Illuminate\Support\Collection包含查询结果的实例，其中每个结果都是 PHPstdClass对象的一个​​实例
+
+        /********************************* 原生sql  **********************************/
+
+//         原生查询的核心方法在  vendor/laravel/framework/src/Illuminate/Database/Connection.php 里面
+        // 教程参考链接：https://laravel.com/docs/8.x/database
+        //该select方法将始终返回一个array结果。数组中的每个结果都是一个 PHPstdClass对象，代表数据库中的一条记录：
+        //查询
+//        $topMenues = DB::select('select * from admin_menu where parent_id = ?', [0]);
+//        $topMenues = DB::select('select * from admin_menu where parent_id = :parent_id', ['parent_id'=>0]); //命名绑定
+//        echo "<pre />"; print_r($topMenues);die;
+
+        //新增
+//       $res =  DB::insert('insert into admin_users (username, password,name) values (?, ?, ?)',
+//            ['wgq', '$2y$10$.NSfFNoWomEOQPSde0qsSeL1unvJY81qo6XjPSJwexqriWqhMRrfK','wgq']);
+//       echo "<pre />"; print_r($res);die;
+
+        //更新
+//        $affected = DB::update( 'update admin_users set name = "star" where name = ?',['wgq'] );
+//        echo "<pre />"; print_r($affected);die;   //成功返回1 ，失败返回0
+
+        //删除
+//        $deleted = DB::delete('delete from admin_users where name = ?',['star']);
+//        echo "<pre />"; print_r($deleted);die; //成功返回1 ，失败返回0
     }
 
 
